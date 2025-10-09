8424684dfaddd03556b6aca3bb2e44401d2141b1
Arun Kishore Voleti <49097688+ArunKishoreVoleti@users.noreply.github.com>
2025-10-09 17:46:45 +0530

    Code Push

--- 176_second_highest_salary.sql
+++ 176_second_highest_salary.sql
@@ -1,4 +1,6 @@
 /*
+Employee table: (id, salary)
+
 This query returns the second highest salary from the Employee table.
 If there is no second highest salary, it returns NULL.
 It uses COUNT(DISTINCT Employee.salary) to check if there are at least two unique salaries.
